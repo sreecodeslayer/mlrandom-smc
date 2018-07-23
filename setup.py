0ec8f437a41e3f2ba900144892e256c07424b7c5
Sreenadh TC <kesav.tc8@gmail.com>
2018-07-23 20:38:44 +0530

    Add initial package setup :tada:

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+REQUIRES = []
+setup(
+    name='mlrandom', version='alpha-0.0.1',
+    description='mlrandom is a simple random text generator for Malayalam',
+    author='Sreenadh TC', author_email='kesav.tc8@gmail.com',
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    zip_safe=False
+)
