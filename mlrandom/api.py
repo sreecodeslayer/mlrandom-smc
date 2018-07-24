f1f52675aa5dda82d40a5137b6c53eec8c17e9ce
Sreenadh TC <kesav.tc8@gmail.com>
2018-07-24 09:59:50 +0530

    Add random word function

--- mlrandom/api.py
+++ mlrandom/api.py
@@ -1,13 +1,23 @@
+import random
 
-DEFAULT_WORDS = [
-    'അ', 'ആ', 'ഇ', 'ഈ', 'ഉ', 'ഊ', 'ഋ', 'ൠ', 'ഌ',
-    'ൡ', 'എ', 'ഏ', 'ഐ', 'ഒ', 'ഓ', 'ഔ', 'ക', 'ഖ',
-    'ഗ', 'ഘ', 'ങ', 'ച', 'ഛ', 'ജ', 'ഝ', 'ഞ', 'ട', 'ഠ',
-    'ഡ', 'ഢ', 'ണ', 'ത', 'ഥ', 'ദ', 'ധ', 'ന', 'പ', 'ഫ', 'ബ',
-    'ഭ', 'മ', 'യ', 'ര', 'ല', 'വ', 'ശ', 'ഷ', 'സ', 'ഹ', 'ള',
-    'ഴ', 'റ', 'ഀ', 'ഁ', 'ം', 'ഃ', 'ാ', 'ി', 'ീ', 'ു', 'ൂ',
-    'ൃ', 'ൄ', 'െ', 'േ', 'ൈ', 'ൊ', 'ോ', 'ൌ', '്', 'ൎ'
-]
+# DEFAULT_WORDS = [
+#     'അ', 'ആ', 'ഇ', 'ഈ', 'ഉ', 'ഊ', 'ഋ', 'ൠ', 'ഌ',
+#     'ൡ', 'എ', 'ഏ', 'ഐ', 'ഒ', 'ഓ', 'ഔ', 'ക', 'ഖ',
+#     'ഗ', 'ഘ', 'ങ', 'ച', 'ഛ', 'ജ', 'ഝ', 'ഞ', 'ട', 'ഠ',
+#     'ഡ', 'ഢ', 'ണ', 'ത', 'ഥ', 'ദ', 'ധ', 'ന', 'പ', 'ഫ', 'ബ',
+#     'ഭ', 'മ', 'യ', 'ര', 'ല', 'വ', 'ശ', 'ഷ', 'സ', 'ഹ', 'ള',
+#     'ഴ', 'റ', 'ഀ', 'ഁ', 'ം', 'ഃ', 'ാ', 'ി', 'ീ', 'ു', 'ൂ',
+#     'ൃ', 'ൄ', 'െ', 'േ', 'ൈ', 'ൊ', 'ോ', 'ൌ', '്', 'ൎ'
+# ]
+DEFAULT_WORDS = dict(
+    vowels=['അ', 'ആ', 'ഇ', 'ഈ', 'ഉ', 'ഊ', 'ഋ', 'ൠ', 'ഌ',
+            'ൡ', 'എ', 'ഏ', 'ഐ', 'ഒ', 'ഓ', 'ഔ'],
+    consonants=['ക', 'ഖ', 'ഗ', 'ഘ', 'ങ', 'ച', 'ഛ', 'ജ', 'ഝ', 'ഞ', 'ട', 'ഠ',
+                'ഡ', 'ഢ', 'ണ', 'ത', 'ഥ', 'ദ', 'ധ', 'ന', 'പ', 'ഫ', 'ബ',
+                'ഭ', 'മ', 'യ', 'ര', 'ല', 'വ', 'ശ', 'ഷ', 'സ', 'ഹ', 'ള',
+                'ഴ', 'റ'],
+    diacs=['ഀ', 'ഁ', 'ം', 'ഃ', 'ാ', 'ി', 'ീ', 'ു', 'ൂ',
+           'ൃ', 'ൄ', 'െ', 'േ', 'ൈ', 'ൊ', 'ോ', 'ൌ', '്', 'ൎ'])
 
 
 class DummyText(object):
@@ -17,7 +27,7 @@ class DummyText(object):
 
     def __init__(self, charset=set(), limit=0, hasnum=False, punctuate=True):
         super(DummyText, self).__init__()
-        self._charset = charset or frozenset(DEFAULT_WORDS)
+        self._charset = charset or DEFAULT_WORDS
         self._limit = limit
         self._text = ''
         self._hasnum = hasnum
@@ -38,3 +48,17 @@ class DummyText(object):
     @property
     def hasnum(self):
         return self._hasnum
+
+    def _gen_word(self, count=1):
+        atleast = random.randint(2, 4)
+        atmost = random.randint(5, 8)
+        wordlen = random.randint(atleast, atmost)
+        word = ''
+        while(len(word) < wordlen):
+            con_dia = '%s%s%s' % (
+                random.choice(self.charset['consonants']),
+                random.choice(self.charset['diacs']),
+                random.choice(self.charset['consonants']),
+            )
+            word += con_dia
+        return word
